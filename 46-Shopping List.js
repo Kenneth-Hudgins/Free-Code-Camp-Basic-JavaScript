dad0fe0a33094383c88754a54bcf2b310ab5a2e7
Kenneth-Hudgins <kennethhudgins21@gmail.com>
2017-11-09 18:23:27 -0600

    Third commit

---
+++ 46-Shopping List.js
@@ -0,0 +1,4 @@
+
+var myList = [["Soylent boxes", 108],["Oatmeal old fashioned", 8],["Dried Cranberries",5],["Crystal Light box", 9],["Coffe pills", 8]];
+
+
---
+++ 50-Local Scope and Functions.js
@@ -0,0 +1,16 @@
+
+function myLocalScope() {
+  'use strict';
+var myVar = 1;
+  
+  
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+
+// Now remove the console log line to pass the test
+
