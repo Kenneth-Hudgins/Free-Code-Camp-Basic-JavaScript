9a7fea41331d5e3b372adb6574c2d62754ee7b11
Kenneth-Hudgins <kennethhudgins21@gmail.com>
2017-11-11 18:23:50 -0600

    Fith commit

---
+++ 79-Build JavaScript Objects.js
@@ -0,0 +1,17 @@
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Spike -Obviously-",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Me", "His Chew Toy"]
+  };
